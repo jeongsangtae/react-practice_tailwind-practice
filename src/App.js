08e44fb8b604f2715b9b12e0d23a9336ea4a6794
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-04 23:37:39 +0900

    example

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
 
       <br />
 
-      <button class="btn btn-green mr-4">Button</button>
-      <button class="btn btn-blue">Button</button>
+      <button className="btn btn-green mr-4">Button</button>
+      <button className="btn btn-blue">Button</button>
 
       <br />
 
-      <button class="bg-red-500 hover:bg-red-700 ...">Hover me</button>
+      <button className="btn btn-red">Hover me</button>
 
       <div className="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
         <p className="text-indigo-600 group-hover:text-gray-900 ...">
@@ -24,6 +24,25 @@ function App() {
         <p className="text-indigo-500 group-hover:text-gray-500 ...">
           Create a new project from a variety of starting templates.
         </p>
+      </div>
+
+      <div className="flex items-center w-3/4 my-5 mt-24">
+        <div className="h-px w-3/4 bg-slate-500 dark:bg-slate-400"></div>
+        <p className="text-center ml-5 mr-1">ABOUT</p>
+        <p className="text-center mr-5 ml-1">ME</p>
+        <div className="h-px w-3/4 bg-slate-500 dark:bg-slate-400"></div>
+      </div>
+
+      <div className="flex items-center w-3/4 my-5">
+        <div className="h-px w-3/4 bg-slate-500 dark:bg-slate-400"></div>
+        <p className="text-center mx-5">CERTIFICATE</p>
+        <div className="h-px w-3/4 bg-slate-500 dark:bg-slate-400"></div>
+      </div>
+
+      <div className="flex items-center w-3/4 my-5">
+        <div className="h-px w-3/4 bg-slate-500 dark:bg-slate-400"></div>
+        <p className="text-center mx-5">SKILL</p>
+        <div className="h-px w-3/4 bg-slate-500 dark:bg-slate-400"></div>
       </div>
     </div>
   );
