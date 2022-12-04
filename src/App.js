173a5316e53baf6ebb375debd644a8f12e7de679
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-04 23:30:22 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "tailwindcss/tailwind.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="">
+      <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+        Hello, Tailwind CSS!
+      </button>
+
+      <br />
+
+      <button class="btn btn-green mr-4">Button</button>
+      <button class="btn btn-blue">Button</button>
+
+      <br />
+
+      <button class="bg-red-500 hover:bg-red-700 ...">Hover me</button>
+
+      <div className="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
+        <p className="text-indigo-600 group-hover:text-gray-900 ...">
+          New Project
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="text-indigo-500 group-hover:text-gray-500 ...">
+          Create a new project from a variety of starting templates.
+        </p>
+      </div>
     </div>
   );
 }
